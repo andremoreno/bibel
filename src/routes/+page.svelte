a3dcb4bed7c1ed55cc14630e75bd5617e6fea2f8
Andre Moreno <elpase@gmail.com>
2022-11-11 01:11:37 +0700

    fix how the home works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import books from "$lib/books.json";
+    import categs from "$lib/categories.json";
 </script>
 
 <svelte:head>
@@ -7,23 +8,13 @@
     <meta name="description" content="Alkitab bahasa Batak Toba.">
 </svelte:head>
 
-<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-    <div>
-        <h3 class="font-bold border-b pb-2 mb-2">Padan Na Robi</h3>
-        {#each books as book}
-            {#if book.category_id == 1}
-            <div class="flex justify-between border-b pb-2 mb-2">
-                <a href="/{book.abbr}">{book.name}</a>
-                {book.number_of_chapters}
-            </div>
-            {/if}
-        {/each}
-    </div>
 
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    {#each categs as categ}
     <div>
-        <h3 class="font-bold border-b pb-2 mb-2">Padan Na Imbaru</h3>
+       <h3 class="font-bold border-b pb-2 mb-2">{categ.name}</h3>
         {#each books as book}
-            {#if book.category_id == 2}
+            {#if book.category_id == categ.id}
             <div class="flex justify-between border-b pb-2 mb-2">
                 <a href="/{book.abbr}">{book.name}</a>
                 {book.number_of_chapters}
@@ -31,4 +22,5 @@
             {/if}
         {/each}
     </div>
+    {/each}
 </div>
